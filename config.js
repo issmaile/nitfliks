c2cf97a57836c0e0dc31cf743305a379a6fe9b5e
Illa Ismail <55895252+issmaile@users.noreply.github.com>
2023-11-17 22:07:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://nitfliks.ismail-illa205.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzQzZDRhMjczMzQxNmEzNDdlYmVmY2MyY2I0YTQ0ZCIsInN1YiI6IjY1NTdjMGFkMjI5MzFhMDBlMjY2NzkxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RE8XCr7MfQzsOrssc-QeXJLAAh0vL5TuGXiuckWrJCA"
 };
